b007cfa67c852d22f8d795fb4a4a4aaff4f0c75d
Saxsori <saxxxsori@gmail.com>
2023-06-15 00:06:13 +0400

    push

---
+++ 01/Dummy.cpp
@@ -0,0 +1,27 @@
+#include "Dummy.hpp"
+
+Dummy::Dummy()
+{
+    this->type = "Target Practice Dummy";
+}
+
+Dummy::Dummy(const Dummy &d)
+{
+    *this = d;
+}
+
+Dummy &Dummy::operator=(const Dummy &d)
+{
+    this->type = d.type;
+    return *this;
+}
+
+Dummy::~Dummy()
+{
+
+}
+
+Dummy *Dummy::clone() const
+{
+    return new Dummy;
+}
---
+++ 02/BrickWall.cpp
@@ -0,0 +1,27 @@
+#include "BrickWall.hpp"
+
+BrickWall::BrickWall()
+{
+    this->type = "Inconspicuous Red-brick Wall";
+}
+
+BrickWall::BrickWall(const BrickWall &d)
+{
+    *this = d;
+}
+
+BrickWall &BrickWall::operator=(const BrickWall &d)
+{
+    this->type = d.type;
+    return *this;
+}
+
+BrickWall::~BrickWall()
+{
+
+}
+
+BrickWall *BrickWall::clone() const
+{
+    return new BrickWall;
+}
